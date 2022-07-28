3f259bc02ced818edbcdc345b8322c75de9b7b78
coolfreewerx <78583528+coolfreewerx@users.noreply.github.com>
2022-07-28 17:31:25 +0700

    lab5-spring-web-api-with-spring-data completed

--- src/main/java/th/ac/ku/menu/repository/MenuRepository.java
+++ src/main/java/th/ac/ku/menu/repository/MenuRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import th.ac.ku.menu.model.Menu;
 
+import java.util.List;
 import java.util.UUID;
 
-@Repository // for connect database
-public interface MenuRepository extends JpaRepository<Menu, UUID> { // jpa จะมี select delete insert or update ให้เลยอัตโนมัติ
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, UUID> {
+    Menu findByName(String name);
+    List<Menu> findByCategory(String category);
 }
-
